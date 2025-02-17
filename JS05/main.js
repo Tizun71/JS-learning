31da056f69d11daa68d90e6743149b14aaddb786
Tizun71 <gnud0701@gmail.com>
2025-02-17 23:33:23 +0700

    JS05

---
+++ JS05/main.js
@@ -0,0 +1,11 @@
+//Input
+let a = Number(prompt("Nhập chiều dài hình chữ nhật:"));
+let b = Number(prompt("Nhập chiều rộng hình chữ nhật:"));
+
+//Caculate
+let chuVi = (a + b) * 2;
+let dienTich = a * b;
+
+//Output
+console.log("Chu vi = " + chuVi);
+console.log("Diện tích = " + dienTich);
