e1cc85547e934d502ced7d0c41c41aa4291c105e
Tizun71 <gnud0701@gmail.com>
2025-02-17 23:54:28 +0700

    JS07

---
+++ JS07/main.js
@@ -0,0 +1,11 @@
+//Input
+let tong = Number(prompt("Nhập tổng:"));
+let hieu = Number(prompt("Nhập hiệu:"));
+
+//Caculate
+let x = (tong + hieu) / 2;
+let y = x - hieu;
+
+//Output
+console.log("x = " + x);
+console.log("y = " + y);
