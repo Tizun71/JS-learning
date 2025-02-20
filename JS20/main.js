c782d784f2aab612c3e693fe9afd4af4cd1ee4c6
Tizun71 <gnud0701@gmail.com>
2025-02-20 09:41:46 +0700

    JS20

---
+++ JS20/main.js
@@ -0,0 +1,12 @@
+for (let n = 1; n <= 1000; n++) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      sum += i;
+    }
+  }
+
+  if (sum == n) {
+    console.log(n);
+  }
+}
